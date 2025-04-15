e4df30c475ffeeb5b0ce5e427cc62deaa40f333c
Nishant <nishant@artenos.io>
2025-04-16 05:25:40 +0530

    Moved to standard dotenv setup.

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,13 +1,7 @@
 import dotenv from "dotenv"
-import path, { dirname } from "path"
 import z from "zod"
-import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-// Load environment variables from .env file, mainly for the development environment
-dotenv.config({ path: path.join(__dirname, "../../.env_dev") })
+dotenv.config()
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]),
