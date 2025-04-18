94fe0ad828a72b7b5611e13d31cbcf7d87d91853
Nishant <nishant@artenos.io>
2025-04-18 20:44:26 +0530

    Added example handler.

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,3 +1,33 @@
-import { resp, apihandler } from "../apibase.js"
+import { resp, apihandler, z } from "../apibase.js"
 
 export const healthcheck = apihandler({ handler: async () => resp(200, "ok") })
+
+// Example of API handler
+const exampleHandler = apihandler({
+  paramsSchema: {
+    id: z.string(),
+  },
+  querySchema: {
+    q: z.string().optional(),
+  },
+  bodySchema: {
+    name: z.string(),
+    age: z.number().int().positive(),
+  },
+  fileSchema: {
+    resume: { maxCount: 2 },
+  },
+  respSchema: {
+    id: z.string(),
+    name: z.string(),
+    age: z.number().int().positive(),
+  },
+  errSchema: {
+    errCode: z.string(),
+    userMsg: z.string(),
+  },
+  handler: async ({ params, query, body, files }) => {
+    // Handle the request here
+    return resp(200, "ok", { id: params.id, name: body.name, age: body.age })
+  },
+})
